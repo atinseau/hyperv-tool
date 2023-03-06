6e18dbc71e57f0b6d3a0c7feb05ee580e4a55e4d
Arthur tinseau <65445747+atinseau@users.noreply.github.com>
2023-03-06 12:48:52 +0000

    fix readme

--- Install.ps1
+++ Install.ps1
@@ -18,10 +18,10 @@ function AutoInstall() {
 AutoInstall
 
 Write-Host "Restart your terminal to use HyperV tools"
-
+Write-Host ""
 Write-Host "  CreateVm  # Create a new VM"
 Write-Host "  SetupVm  # Auto setup a VM with docker,node, git, yarn, pnpm, etc..."
 Write-Host "  UpdateAddress # update all host ip and vm ip usage"
 Write-Host "  DeployUpdate -?Only <update_id> -?Ignore <update_ids[],>  # Deploy new update, script, package in your vm"
-
+Write-Host
 Write-Host "Done! You can now run hyper v commands from any directory."
