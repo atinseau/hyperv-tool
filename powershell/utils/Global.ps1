3d8d5dcde6c87f9b71a49c95804ee2c80fa88e13
tinseau <arthur.tinseau.catamania@ogf.fr>
2023-03-06 15:37:19 +0100

    syncro

--- powershell/utils/Global.ps1
+++ powershell/utils/Global.ps1
@@ -3,14 +3,14 @@
 . "$PSScriptRoot\Functions.ps1"
 
 function CheckUpdate () {
-    $currentPath = (Get-Location).Path
-    Set-Location $gitDirectory
+    $currentPath = (Get-Location ).Path
+    Set-Location $gitDirectory | Out-Null
     git fetch 
     $diff = git diff master...origin/master
     if ($diff) {
         git pull
     }
-    Set-Location $currentPath
+    Set-Location $currentPath | Out-Null
     if ($diff) {
         Write-Host "[Script reloaded]" -ForegroundColor Green
         $currentScript = $MyInvocation.ScriptName
