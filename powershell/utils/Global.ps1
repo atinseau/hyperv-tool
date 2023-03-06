b68f4b0e387595fd1c5546f197ef1d1d87f5a377
tinseau <arthur.tinseau.catamania@ogf.fr>
2023-03-06 14:58:16 +0100

    fix

--- powershell/utils/Global.ps1
+++ powershell/utils/Global.ps1
@@ -2,4 +2,14 @@
 . "$PSScriptRoot\Variables.ps1"
 . "$PSScriptRoot\Functions.ps1"
 
+git fetch
+$diff = git diff master...origin/master
+
+if ($diff) {
+    Write-Output "There are changes in the remote repository"
+    Write-Output "Updating local repository"
+    git pull
+    Write-Output "Local repository updated"
+}
+
 Write-Output "Global variables and functions loaded"
