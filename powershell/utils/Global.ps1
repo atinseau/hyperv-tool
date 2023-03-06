3d6b4f532f06f164f5e766c0d91379bc6fe6b8cc
tinseau <arthur.tinseau.catamania@ogf.fr>
2023-03-06 14:59:45 +0100

    fix

--- powershell/utils/Global.ps1
+++ powershell/utils/Global.ps1
@@ -4,10 +4,7 @@
 
 git fetch
 $diff = git diff master...origin/master
-
 if ($diff) {
-    Write-Output "There are changes in the remote repository"
-    Write-Output "Updating local repository"
     git pull
-    Write-Output "Local repository updated"
+    exit
 }
