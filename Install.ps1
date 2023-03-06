c02322b1fc9ecf769a1b0cf59277d43bc0f8515a
tinseau <arthur.tinseau.catamania@ogf.fr>
2023-03-06 12:29:42 +0100

    implement auto install

--- Install.ps1
+++ Install.ps1
@@ -9,8 +9,19 @@ function AddPath($Path) {
   $Env:Path = $Path
 }
 
-$currentPath = (Get-Location).Path
+function AutoInstall() {
+  git clone "git@github.com:atinseau/hyperv-tool.git"
+  $currentPath = (Get-Location).Path
+  AddPath -Path "$currentPath\powershell"
+}
 
-AddPath -Path "$currentPath\powershell"
+AutoInstall
+
+Write-Host "Restart your terminal to use HyperV tools"
+
+Write-Host "  CreateVm  # Create a new VM"
+Write-Host "  SetupVm  # Auto setup a VM with docker,node, git, yarn, pnpm, etc..."
+Write-Host "  UpdateAddress # update all host ip and vm ip usage"
+Write-Host "  DeployUpdate -?Only <update_id> -?Ignore <update_ids[],>  # Deploy new update, script, package in your vm"
 
 Write-Host "Done! You can now run hyper v commands from any directory."
